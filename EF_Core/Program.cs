d158890ea872cb96ff6e420a69da88dc9e16941f
Safa Can Cevik <safa90462@ogr.duzce.edu.tr>
2023-06-13 16:44:44 +0300

    lesson9

--- EF_Core/Program.cs
+++ EF_Core/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hi");
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using System.IO;
+
+Console.WriteLine("Hi");
+
+//EF CORE
+
+//•	DbContext: veritabanını temsil edecek olan sınıftır. Ve DbContext’ten türetilmelidir. Temel yapılandırmalardan sorumludur. Sorgulama operasyonlarını yürütür. SQL dönüşümü ve veritabanına gönderme yapar.
+//•	Entity: tablolarımı temsil edecek sınıf. Tablolar çoğul iken Entity’ler tekil olur.
+//•	Entityler’in DbSet’teki adları çoğuldur. Tablolara denktir. 
+//•	DBFirst’te önce veritabanı oluşturulur. Sonrasında scaffold ile gerçekleştiriz.
+//•	CodeFirst: Code kısmındakini aktarmak için Migration şart.
+//•	Migration: Code kısmında oluşturduklarımızı database’nin anlayacağı hale getiren bir c# class’tır.
+//•	OnConfiguring: Temel ayarlama yapmamızı sağlar.
+//•	Migration sonucunda Migrations klasörü içerisinde ismini verdiğimiz migration içerisinde Up ve Down vardır. Tabloyu Up oluşturmayı sağlarken Down ise silmeyi sağlar.
+//•	Migration oluşturduktan sonra update-database ile migrate ediyoruz. Birden fazla migration varsa Db içerisinde History diye table oluşturulur. İstenilen seviyede database’yi çalıştırabiliriz.
+//•	Kod üzerinde de migrate operasyonunu yapabiliyoruz. Bunun için de DbContext’ten nesne oluşturup await context.Database.MigrateAsync(); ile uygulama ayağa kalktığı zaman database’yi oluşturabiliriz.
+//•	Yapılan değişiklikler kesinlikle manuel olarak veritabanında yapılmaz. Önce kod kısmında yapılıp sonrasında migration edilir. Sonrasında da migrate edilerek veritabanına yansıtılır.
+//•	Migration için komutlar:
+//add - migration[migration name]
+//add - migration[migration name] - OutputDir[path]
+//remove - migration
+//update - database => up etme
+//update-database  [migration name] => down etme
---
+++ Lesson9/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+Console.WriteLine("Hello, World!");
+
+public class ECommerceDbContext: DbContext
+{
+    public DbSet<Product> Products  { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=CAN\SQLEXPRESS; Database=ECommerceDb; User Id=sa; Password=Windowserver19*");
+
+        //Provider,
+        //ConnectionString,
+        //Lazy Loading,
+        //vb configurations
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    
+    // Basic aşamada default olarak Id içeren (tabloadiID,tabloadiId,Id,ID gibi) primary key olarak alır.
+    // PK olmadan entity olmaz.
+}
