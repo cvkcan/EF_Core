a520d071ec765442e028105dce34becd8ad0a9b1
Safa Can Cevik <safa90462@ogr.duzce.edu.tr>
2023-06-13 16:11:58 +0300

    Add project files.

---
+++ EF_Core/Migrations/20230613105427_init3.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class init3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Customers",
+                newName: "Assa");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Assa",
+                table: "Customers",
+                newName: "FirstName");
+        }
+    }
+}
---
+++ EF_Core/Migrations/20230613105502_init4.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class init4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
